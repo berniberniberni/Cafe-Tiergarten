cf484b86fdef6752651c9d939ae3512bd5763db6
Bernadette Kretzschmar <bernadettekretzschmar@MacBookAir.fritz.box>
2025-04-27 17:22:51 +0200

    first commit

---
+++ cafetiergarten1/site/snippets/footer.php
@@ -0,0 +1,6 @@
+ 
+        <footer class="footer">
+        <a href="<?= $site->instagram()->esc() ?>" target="_blank" rel="noopener noreferrer">Follow us on Instagram</a>
+        </footer>
+    </body>
+</html>
