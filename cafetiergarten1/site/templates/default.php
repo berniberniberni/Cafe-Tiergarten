e7e7e3403f605251794d5c6fd715e7c673ac3352
Bernadette Kretzschmar <bernadettekretzschmar@MacBookAir.fritz.box>
2025-04-28 12:16:58 +0200

    footer and nav corrected

--- cafetiergarten1/site/templates/default.php
+++ cafetiergarten1/site/templates/default.php
@@ -1,11 +1,16 @@
-<?php snippet('header') ?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <?php snippet('header') ?>
+</head>
+
 
 <body>
   <?php snippet('sidebar') ?>
 
   <div class="page-container">
     <header class="header">
-      <h1><?= $page->title() ?></h1>
+      <h1><?= $page->title()->html() ?></h1>
     </header>
 
     <main class="main-content">
