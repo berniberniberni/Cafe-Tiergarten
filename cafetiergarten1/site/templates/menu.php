9e8ea168bafc71206b4c20b4800124dd98f0701d
Bernadette Kretzschmar <145860513+berniberniberni@users.noreply.github.com>
2025-07-20 23:03:10 +0200

    Apply latest design updates

--- cafetiergarten1/site/templates/menu.php
+++ cafetiergarten1/site/templates/menu.php
@@ -6,29 +6,31 @@
     <h1><?= $page->title() ?></h1>
   </header>
 
-  <main class="main-content scroll menu">
-    <div class="text-column">
-      <h2>Getränke</h2>
-      <ul class="menu-list">
-        <?php foreach ($page->drinks()->toStructure() as $drink): ?>
-          <li>
-            <strong><?= $drink->name() ?></strong> – <?= $drink->price() ?><br>
-            <small><?= $drink->description() ?></small>
-          </li>
-        <?php endforeach ?>
-      </ul>
+  <main class="main-content menu">
+  <div class="text-column left-page">
+    <h2>Getränke</h2>
+    <ul class="menu-list">
+      <?php foreach ($page->drinks()->toStructure() as $drink): ?>
+        <li>
+          <strong><?= $drink->name() ?></strong> – <?= $drink->price() ?><br>
+          <small><?= $drink->description() ?></small>
+        </li>
+      <?php endforeach ?>
+    </ul>
+  </div>
 
-      <h2>Speisen</h2>
-      <ul class="menu-list">
-        <?php foreach ($page->food()->toStructure() as $food): ?>
-          <li>
-            <strong><?= $food->name() ?></strong> – <?= $food->price() ?><br>
-            <small><?= $food->description() ?></small>
-          </li>
-        <?php endforeach ?>
-      </ul>
-    </div>
-  </main>
+  <div class="text-column right-page">
+    <h2>Speisen</h2>
+    <ul class="menu-list">
+      <?php foreach ($page->food()->toStructure() as $food): ?>
+        <li>
+          <strong><?= $food->name() ?></strong> – <?= $food->price() ?><br>
+          <small><?= $food->description() ?></small>
+        </li>
+      <?php endforeach ?>
+    </ul>
+  </div>
+</main>
 
   <?php snippet('footer') ?>
 </div>
