cd53eecc094dad1223bd3c337234179f726f93e9
Bernadette Kretzschmar <145860513+berniberniberni@users.noreply.github.com>
2025-08-14 22:05:39 +0200

    menu page

--- cafetiergarten1/site/templates/menu.php
+++ cafetiergarten1/site/templates/menu.php
@@ -1,36 +1,47 @@
 <?php snippet('header') ?>
-<?php snippet('sidebar') ?>
 
-<div class="page-container">
-  <header class="header">
-    <h1><?= $page->title() ?></h1>
-  </header>
+  <?php snippet('sidebar') ?>
+  <div class="page-container">
+    <header class="header">
+      <h1><?= $page->title()->html() ?></h1>
+    </header>
 
-  <main class="main-content menu">
-  <div class="text-column left-page">
-    <h2>Getränke</h2>
-    <ul class="menu-list">
-      <?php foreach ($page->drinks()->toStructure() as $drink): ?>
-        <li>
-          <strong><?= $drink->name() ?></strong> – <?= $drink->price() ?><br>
-          <small><?= $drink->description() ?></small>
-        </li>
-      <?php endforeach ?>
-    </ul>
-  </div>
+    <main class="main-content two-columns">
 
-  <div class="text-column right-page">
-    <h2>Speisen</h2>
-    <ul class="menu-list">
-      <?php foreach ($page->food()->toStructure() as $food): ?>
-        <li>
-          <strong><?= $food->name() ?></strong> – <?= $food->price() ?><br>
-          <small><?= $food->description() ?></small>
-        </li>
-      <?php endforeach ?>
-    </ul>
-  </div>
-</main>
+    <div class="text-column scroll">
+  <h2>Getränke</h2>
+  <ul class="menu-list">
+    <?php foreach ($page->drinks()->toStructure() as $drink): ?>
+      <li>
+        <strong><?= $drink->name() ?></strong> – <?= $drink->price() ?><br>
+        <small><?= $drink->description() ?></small>
+      </li>
+    <?php endforeach ?>
+  </ul>
 
-  <?php snippet('footer') ?>
+  <h2>Speisen</h2>
+  <ul class="menu-list">
+    <?php foreach ($page->food()->toStructure() as $food): ?>
+      <li>
+        <strong><?= $food->name() ?></strong> – <?= $food->price() ?><br>
+        <small><?= $food->description() ?></small>
+      </li>
+    <?php endforeach ?>
+  </ul>
 </div>
+
+      <div class="image-column scroll">
+        <?php foreach ($page->children()->listed() as $section): ?>
+          <?php foreach ($section->images() as $image): ?>
+            <img src="<?= $image->url() ?>" id="<?= $image->name() ?>" alt="<?= $image->alt()->or($image->filename()) ?>">
+          <?php endforeach ?>
+        <?php endforeach ?>
+      </div>
+
+    </main>
+
+    <?php snippet('footer') ?>
+  </div>
+</body>
+</html>
+
