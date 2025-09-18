53ef1e0e57bee664b166129e4740f86ab7324b96
Bernadette Kretzschmar <145860513+berniberniberni@users.noreply.github.com>
2025-09-18 10:54:00 +0200

    hours changed

--- cafetiergarten1/assets/js/status.js
+++ cafetiergarten1/assets/js/status.js
@@ -3,13 +3,13 @@ function checkIfOpen() {
   const day = now.getDay(); // 0 = So, 6 = Sa
   const hour = now.getHours();
 
-  // Mi (3) bis So (0)
-  const isOpenDay = (day >= 3 && day <= 6) || day === 0;
+  // Di (2) bis So (0)
+  const isOpenDay = (day >= 2 && day <= 6) || day === 0;
 
-  let message = 'Geschlossen. Bitte komm Mi - So zwischen 10-21 Uhr wieder.';
+  let message = 'Geschlossen. Komm Di, Mi, So zwischen 10-18 Uhr und Fr, Sa zwischen 10-22 Uhr wieder.';
 
   if (isOpenDay && hour >= 11 && hour < 18) {
-    message = "Heute geöffnet bis 21 Uhr.";
+    message = "Heute geöffnet bis 18 Uhr.";
   }
 
   const el = document.getElementById("statusMessage");
